cd89cb1dae945e1ef11b99c4f103771f9d52e7a5
Иван <ivan_0805@mail.ru>
2014-11-22 16:41:50 +0300

    add button and openning file manager

--- src/ru/project/dnareader/MainActivity.java
+++ src/ru/project/dnareader/MainActivity.java
@@ -1,15 +1,101 @@
 package ru.project.dnareader;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
+	Button btn;
+	
     protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	TextView tv = new TextView(this);
-        tv.setText(WorkingWithJni.stringFromJNI());
-        setContentView(tv);
-    }    
+    	setContentView(R.layout.main);
+//    	TextView tv = new TextView(this);
+//        tv.setText(WorkingWithJni.stringFromJNI());
+//        setContentView(tv);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+        }
+        
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()){
+		case R.id.btn:
+			showFileChooser();
+			break;
+		}
+		
+	}
+   
+    private static final int FILE_SELECT_CODE = 0;
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT); 
+        intent.setType("*/*"); 
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.", 
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    /*You would then listen for the selected file's Uri in onActivityResult() 
+    *like so:
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+            if (resultCode == RESULT_OK) {
+                // Get the Uri of the selected file 
+                Uri uri = data.getData();
+                Log.d("TAG", "File Uri: " + uri.toString());
+                // Get the path
+                String path = FileUtils.getPath(this, uri);
+                Log.d("TAG", "File Path: " + path);
+                // Get the file instance
+                // File file = new File(path);
+                // Initiate the upload
+            }
+            break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+   // The getPath() method in my FileUtils.java is:
+
+    public static String getPath(Context context, Uri uri) throws URISyntaxException {
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = { "_data" };
+            Cursor cursor = null;
+
+            try {
+                cursor = context.getContentResolver().query(uri, projection, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow("_data");
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+                // Eat it
+            }
+        }
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    } */
+
+	
 }
